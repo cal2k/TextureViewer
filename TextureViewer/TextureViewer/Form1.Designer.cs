b15e3b73554ace90f1012dc4d2a93f26b15236fd
Calum <CalumJefferiss89@gmail.com>
2020-10-04 21:55:32 +0100

    0.0.1

---
+++ TextureViewer/TextureViewer/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace TextureViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbMaterials = new System.Windows.Forms.ListBox();
+            this.lbOptions = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSelectLibarary = new System.Windows.Forms.Button();
+            this.lblCurrentDir = new System.Windows.Forms.Label();
+            this.btnCC0 = new System.Windows.Forms.Button();
+            this.btnOpenImage = new System.Windows.Forms.Button();
+            this.btnTextureLocation = new System.Windows.Forms.Button();
+            this.btnCopyTexturePath = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbMaterials
+            // 
+            this.lbMaterials.FormattingEnabled = true;
+            this.lbMaterials.Location = new System.Drawing.Point(15, 38);
+            this.lbMaterials.Name = "lbMaterials";
+            this.lbMaterials.Size = new System.Drawing.Size(107, 433);
+            this.lbMaterials.TabIndex = 0;
+            this.lbMaterials.SelectedIndexChanged += new System.EventHandler(this.lbMaterials_SelectedIndexChanged);
+            // 
+            // lbOptions
+            // 
+            this.lbOptions.FormattingEnabled = true;
+            this.lbOptions.HorizontalScrollbar = true;
+            this.lbOptions.Location = new System.Drawing.Point(128, 38);
+            this.lbOptions.Name = "lbOptions";
+            this.lbOptions.Size = new System.Drawing.Size(170, 433);
+            this.lbOptions.TabIndex = 1;
+            this.lbOptions.SelectedIndexChanged += new System.EventHandler(this.lbOptions_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(304, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(484, 404);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnSelectLibarary
+            // 
+            this.btnSelectLibarary.Location = new System.Drawing.Point(15, 9);
+            this.btnSelectLibarary.Name = "btnSelectLibarary";
+            this.btnSelectLibarary.Size = new System.Drawing.Size(107, 23);
+            this.btnSelectLibarary.TabIndex = 3;
+            this.btnSelectLibarary.Text = "Select Library";
+            this.btnSelectLibarary.UseVisualStyleBackColor = true;
+            this.btnSelectLibarary.Click += new System.EventHandler(this.btnSelectLibarary_Click);
+            // 
+            // lblCurrentDir
+            // 
+            this.lblCurrentDir.Location = new System.Drawing.Point(125, 14);
+            this.lblCurrentDir.Name = "lblCurrentDir";
+            this.lblCurrentDir.Size = new System.Drawing.Size(582, 21);
+            this.lblCurrentDir.TabIndex = 4;
+            // 
+            // btnCC0
+            // 
+            this.btnCC0.Location = new System.Drawing.Point(713, 9);
+            this.btnCC0.Name = "btnCC0";
+            this.btnCC0.Size = new System.Drawing.Size(75, 23);
+            this.btnCC0.TabIndex = 5;
+            this.btnCC0.Text = "cc0Textures";
+            this.btnCC0.UseVisualStyleBackColor = true;
+            this.btnCC0.Click += new System.EventHandler(this.btnCC0_Click);
+            // 
+            // btnOpenImage
+            // 
+            this.btnOpenImage.Enabled = false;
+            this.btnOpenImage.Location = new System.Drawing.Point(659, 448);
+            this.btnOpenImage.Name = "btnOpenImage";
+            this.btnOpenImage.Size = new System.Drawing.Size(129, 23);
+            this.btnOpenImage.TabIndex = 6;
+            this.btnOpenImage.Text = "View Texture Image";
+            this.btnOpenImage.UseVisualStyleBackColor = true;
+            this.btnOpenImage.Click += new System.EventHandler(this.btnOpenImage_Click);
+            // 
+            // btnTextureLocation
+            // 
+            this.btnTextureLocation.Location = new System.Drawing.Point(304, 448);
+            this.btnTextureLocation.Name = "btnTextureLocation";
+            this.btnTextureLocation.Size = new System.Drawing.Size(129, 23);
+            this.btnTextureLocation.TabIndex = 7;
+            this.btnTextureLocation.Text = "Open Texture Location";
+            this.btnTextureLocation.UseVisualStyleBackColor = true;
+            this.btnTextureLocation.Click += new System.EventHandler(this.btnTextureLocation_Click);
+            // 
+            // btnCopyTexturePath
+            // 
+            this.btnCopyTexturePath.Location = new System.Drawing.Point(439, 448);
+            this.btnCopyTexturePath.Name = "btnCopyTexturePath";
+            this.btnCopyTexturePath.Size = new System.Drawing.Size(129, 23);
+            this.btnCopyTexturePath.TabIndex = 8;
+            this.btnCopyTexturePath.Text = "Copy Texture Path";
+            this.btnCopyTexturePath.UseVisualStyleBackColor = true;
+            this.btnCopyTexturePath.Click += new System.EventHandler(this.btnCopyTexturePath_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 483);
+            this.Controls.Add(this.btnCopyTexturePath);
+            this.Controls.Add(this.btnTextureLocation);
+            this.Controls.Add(this.btnOpenImage);
+            this.Controls.Add(this.btnCC0);
+            this.Controls.Add(this.lblCurrentDir);
+            this.Controls.Add(this.btnSelectLibarary);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbOptions);
+            this.Controls.Add(this.lbMaterials);
+            this.Name = "Form1";
+            this.Text = "Texture Preview";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbMaterials;
+        private System.Windows.Forms.ListBox lbOptions;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSelectLibarary;
+        private System.Windows.Forms.Label lblCurrentDir;
+        private System.Windows.Forms.Button btnCC0;
+        private System.Windows.Forms.Button btnOpenImage;
+        private System.Windows.Forms.Button btnTextureLocation;
+        private System.Windows.Forms.Button btnCopyTexturePath;
+    }
+}
+
---
+++ TextureViewer/TextureViewer/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TextureViewer
+{
+    public partial class Form1 : Form
+    {
+        string TexturePath, CurrentPath, CurrentMaterial, CurrentTexture, FileName, imagePath, temp;
+        String[] dirs, tempArray;
+        int i, ii;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void btnSelectLibarary_Click(object sender, EventArgs e)
+        {
+            lbMaterials.Items.Clear();
+
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    TexturePath = fbd.SelectedPath.ToString();
+                    lblCurrentDir.Text = TexturePath;
+                    ii = TexturePath.Count();
+
+                    loadMaterials();
+                }
+            }
+        }
+        
+        private void btnCC0_Click(object sender, EventArgs e)
+        {
+            lbMaterials.Items.Clear();
+
+            TexturePath = "d:\\Documents\\Assits\\Textures\\cc0textures.com";
+            lblCurrentDir.Text = TexturePath;
+            ii = TexturePath.Count();
+
+            loadMaterials();
+        }
+
+        private void loadMaterials()
+        {
+            btnOpenImage.Enabled = false;
+            btnTextureLocation.Enabled = false;
+
+            dirs = Directory.GetDirectories(TexturePath);
+            for (i = 0; i < dirs.Length; i++)
+            {
+                temp = dirs[i];
+                temp = temp.Remove(0, ii + 1);
+                lbMaterials.Items.Add(temp);
+            }
+        }
+        private void lbMaterials_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CurrentMaterial = lbMaterials.SelectedItem.ToString();
+            CurrentPath = TexturePath + "\\" + CurrentMaterial;
+            pictureBox1.Image = null;
+            loadOptions();
+            
+        }
+
+        private void loadOptions()
+        {
+            dirs = System.IO.Directory.GetDirectories(CurrentPath);
+
+            lbOptions.Items.Clear();
+            btnOpenImage.Enabled = false;
+            btnTextureLocation.Enabled = false;
+
+            for (i = 0; i < dirs.Length; i++)
+            {
+                temp = dirs[i];
+                temp = temp.Remove(0, ii + 1);
+                tempArray = temp.Split('\\');
+                lbOptions.Items.Add(tempArray[1]);
+            }
+        }
+        private void lbOptions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CurrentTexture = lbOptions.SelectedItem.ToString();
+            tempArray = CurrentTexture.Split('-');
+            FileName = tempArray[0];
+            loadImage();
+        }
+
+        private void loadImage()
+        {
+            try
+            {
+                imagePath = (CurrentPath + "\\" + CurrentTexture + "\\" + FileName + "_Color.jpg");
+                pictureBox1.Load(imagePath);
+                btnOpenImage.Enabled = true;
+                btnTextureLocation.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    imagePath = (CurrentPath + "\\" + CurrentTexture + "\\" + FileName + "_Color.png");
+                    pictureBox1.Load(imagePath);
+                    btnOpenImage.Enabled = true;
+                    btnTextureLocation.Enabled = true;
+                }
+                catch (Exception exx)
+                {
+                    MessageBox.Show(ex.ToString());
+                    MessageBox.Show(exx.ToString());
+                }
+            }
+        }
+
+        private void btnOpenImage_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(imagePath);
+        }
+        private void btnTextureLocation_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(CurrentPath + "\\" + CurrentTexture);
+        }
+        private void btnCopyTexturePath_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Clipboard.SetText(CurrentPath + "\\" + CurrentTexture);
+        }
+    }
+}
